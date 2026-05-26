f63f5c7e25c2a0973af852d376565a5eeec6d35d
glsunnykumar <glsunny.kumar@gmail.com>
2026-05-26 12:22:59 +0530

    adding a login page

--- src/app/services/auth/auth.ts
+++ src/app/services/auth/auth.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {
+export class AuthService  {
+
+   private auth = inject(Auth);
+
+   user$ = authState(this.auth);  ;
+
+  // Register
+  register(email: string, password: string) {
+    return createUserWithEmailAndPassword(
+      this.auth,
+      email,
+      password
+    );
+  }
+
+  // Login
+  login(email: string, password: string) {
+    return signInWithEmailAndPassword(
+      this.auth,
+      email,
+      password
+    );
+  }
+
+    // Logout
+  logout() {
+    return signOut(this.auth);
+  }
+
+  // Current User
+  get currentUser() {
+    return this.auth.currentUser;
+  }
   
 }
