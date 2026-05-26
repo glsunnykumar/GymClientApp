33ab043cdaa298cbc0da304d739602e9df6c0860
glsunnykumar <glsunny.kumar@gmail.com>
2026-05-26 15:20:13 +0530

    testing the component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.page')
+        .then(m => m.LoginPage)
   },
+
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  }
 ];
